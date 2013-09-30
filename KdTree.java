4c7d2b0ac66a77e133b966052b279f287aed3eba
Revil <leowang137@gmail.com>
2013-09-30 16:37:14 +0800

    initial assignment interfaces

---
+++ KdTree.java
@@ -0,0 +1,31 @@
+
+
+public class KdTree {
+
+    // construct an empty set of points
+    public KdTree() {}
+
+    // is the set empty?
+    public boolean isEmpty() { return true; }
+
+    // number of points in the set
+    public int size() { return 0; }
+
+    // add the point p to the set (if it is not already in the set)
+    public void insert(Point2D p) {}
+
+    // does the set contain the point p?
+    public boolean contains(Point2D p) { return false; }
+
+    // draw all of the points to standard draw
+    public void draw() {}
+
+    // all points in the set that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) { return null; }
+
+    // a nearest neighbor in the set to p: null if set is empty
+    // proportional to N
+    public Point2D nearest(Point2D p) { return null; }
+
+    public static void main(String[] args) {}
+}
