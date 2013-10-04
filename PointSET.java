3cf48e0529b7c690e2e0af49d987e33637771e2f
Revil <leowang137@gmail.com>
2013-10-04 11:28:10 +0800

    complete KdTree.java and PointSET.java
    KdTree.java with RectHV object implemented

--- PointSET.java
+++ PointSET.java
@@ -1,33 +1,64 @@
 
 public class PointSET {
 
+    private SET<Point2D> set;
+
     // construct an empty set of points
-    public PointSET() {}
+    public PointSET() { set = new SET<Point2D>(); }
 
     // is the set empty?
-    public boolean isEmpty() { return true; }
+    public boolean isEmpty() { return set.isEmpty(); }
 
     // number of points in the set
-    public int size() { return 0; }
+    public int size() { return set.size(); }
 
     // add the point p to the set (if it is not already in the set)
     // proportional to logarithm of N in the worst case
-    public void insert(Point2D p) {}
+    public void insert(Point2D p) { set.add(p); }
 
     // does the set contain the point p?
     // proportional to logarithm of N in the worst case
-    public boolean contains(Point2D p) { return false; }
+    public boolean contains(Point2D p) { return set.contains(p); }
 
     // draw all of the points to standard draw
-    public void draw() {}
+    public void draw() {
+         StdDraw.setPenColor(StdDraw.BLACK);
+         StdDraw.setPenRadius(.01);
+
+         for (Point2D p : set)
+             StdDraw.point(p.x(), p.y());
+
+         StdDraw.show(0);
+    }
 
     // all points in the set that are inside the rectangle
     // proportional to N in the worst case
-    public Iterable<Point2D> range(RectHV rect) { return null; }
+    public Iterable<Point2D> range(RectHV rect) {
+        Stack<Point2D> s = new Stack<Point2D>();
+
+        for (Point2D p : set)
+            if (rect.contains(p))
+                s.push(p);
+
+        return s;
+    }
 
     // a nearest neighbor in the set to p: null if set is empty
     // proportional to N
-    public Point2D nearest(Point2D p) { return null; }
+    public Point2D nearest(Point2D p) {
+        if (isEmpty()) return null;
 
-    public static void main(String[] args) {}
+        double dis, minDis = Double.MAX_VALUE;
+        Point2D q = null;
+
+        for (Point2D ip : set) {
+            dis = p.distanceSquaredTo(ip);
+            if (dis < minDis) {
+                minDis = dis;
+                q = ip;
+            }
+        }
+
+        return q;
+    }
 }
